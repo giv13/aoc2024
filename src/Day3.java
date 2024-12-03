72120e6a6eb4f6badbd236759409670494d7f0c9
Ilia Grigorev <giv13@bk.ru>
2024-12-03 16:04:00 +0500

    Day 3 completed

---
+++ src/Day3.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Day3 {
+    private final String filePath;
+    private final List<String> corruptedMemory = new ArrayList<>();
+
+    public Day3(String filePath) {
+        this.filePath = filePath;
+        readFile();
+    }
+
+    private void readFile() {
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(filePath));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                corruptedMemory.add(line);
+            }
+            reader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден!");
+        } catch (IOException e) {
+            System.out.println("Ошибка ввода/вывода!");
+        }
+    }
+
+    public int multiplications(String regex) {
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher;
+        boolean isDo = true;
+        int result = 0;
+        for (String corruptedLine : corruptedMemory) {
+            matcher = pattern.matcher(corruptedLine);
+            while (matcher.find()) {
+                switch (matcher.group(1)) {
+                    case "do" -> isDo = true;
+                    case "don't" -> isDo = false;
+                    case "mul" -> result += isDo ? Integer.parseInt(matcher.group(2)) * Integer.parseInt(matcher.group(3)) : 0;
+                }
+            }
+        }
+        return result;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -7,5 +7,9 @@ public class Main {
         Day2 day2 = new Day2("files/day2.txt");
         System.out.println(day2.safeReportsCount());
         System.out.println(day2.safeReportsCount(1));
+
+        Day3 day3 = new Day3("files/day3.txt");
+        System.out.println(day3.multiplications("(mul)\\((\\d+),(\\d+)\\)"));
+        System.out.println(day3.multiplications("(mul(?=\\(\\d+,\\d+\\))|do|don't)\\((?:(\\d+),(\\d+))?\\)"));
     }
 }
